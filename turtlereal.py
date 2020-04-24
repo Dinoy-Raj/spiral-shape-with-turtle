79acf37830bf322b1f8930dc4a1676ffe733f48f
Dinoy Raj <62199728+Dinoy-Raj@users.noreply.github.com>
2020-04-24 23:19:24 +0530

    Create turtlereal.py
    create spiral network with jumping turtles

---
+++ turtlereal.py
@@ -0,0 +1,18 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("green")
+
+elan = turtle.Turtle()
+elan.color("blue")
+elan.shape("turtle")
+
+elan.up()
+distance = 5
+an=24
+for _ in range(70):
+    elan.stamp()
+    elan.forward(distance)
+    elan.right(an)
+
+    distance = distance + 2
+wn.exitonclick()
